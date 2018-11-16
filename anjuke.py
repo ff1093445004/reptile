0885e9df56f338865eb592b416a0d7415e99426e
root <gogs@gogs.com>
2018-11-16 14:38:43 +0800

    last

--- anjuke.py
+++ anjuke.py
@@ -3,7 +3,6 @@ import requests
 import json
 from lxml import etree
 
-
 if __name__ == '__main__':
     target = 'https://nj.fang.anjuke.com/loupan/s?kw='
     headers = {
@@ -17,8 +16,8 @@ if __name__ == '__main__':
         item = {"name": div.xpath(".//h3/span[@class='items-name']/text()"),
                 "price": div.xpath(".//p[@class='price']//text()")}
         item_list.append(item)
-    
-    with open("wj.txt", "a" ,encoding="utf-8") as f:
+
+    with open("wj.txt", "a", encoding="utf-8") as f:
         for item in item_list:
             f.write(json.dumps(item, ensure_ascii=False))
             f.write("\n")
